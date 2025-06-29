fe455b45049f0a2135c9b779a4df24c7c32d1185
physerkm <erkam.sis@outlook.com>
2025-06-29 23:53:00 +0300

    v5 - Refactor Form Submission

---
+++ React State/v5-Refactor-form-submission/refactor-form-submission/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header.jsx";
+import Main from "./Main.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
---
+++ React State/v5-Refactor-form-submission/refactor-form-submission/src/Main.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Main() {
+  /**
+   * Challenge: Use form action instead of onSubmit to
+   * handle the data from the form
+   */
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.currentTarget);
+  //   const newIngredient = formData.get("ingredient");
+  //   setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  // }
+
+  return (
+    <main>
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e. g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      <ul>{ingredientsListItems}</ul>
+    </main>
+  );
+}
