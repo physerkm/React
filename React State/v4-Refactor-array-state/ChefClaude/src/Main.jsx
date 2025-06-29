0e84201d8dc420b297e8b005b9e37defd9d647e4
physerkm <erkam.sis@outlook.com>
2025-06-29 23:52:10 +0300

    v4 - Refactor Array State

---
+++ React State/v4-Refactor-array-state/ChefClaude/src/Main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Main() {
+  /**
+   * Challenge: Update our app so that when the user enters a
+   * new ingredient and submits the form, it adds that new
+   * ingredient to our list!
+   */
+
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    /**
+     * Like before, don't worry about this FormData stuff yet.
+     * Just use the newIngredient below to help you finish the
+     * challenge.
+     */
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      <ul>{ingredientsListItems}</ul>
+    </main>
+  );
+}
