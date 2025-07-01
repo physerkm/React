f6c56d68f602d13e6f8772ec049a792f6667647e
physerkm <erkam.sis@outlook.com>
2025-07-01 23:32:15 +0300

    useState

---
+++ React State/useState/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function App() {
+  /**
+   * Challenge: Replace our hard-coded "Yes" on the page with
+   * some state initiated with React.useState()
+   */
+  const result = React.useState("Yes");
+  console.log(result);
+
+  return (
+    <main>
+      <h1 className="title">Is state important to know?</h1>
+      <button className="value">{result[0]}</button>
+    </main>
+  );
+}
