58acb6fa35f179b1b57eafca6a2d58a07524be23
physerkm <erkam.sis@outlook.com>
2025-07-07 23:11:40 +0300

    Conditional Rendering Practice

---
+++ React State/conditional-rendering-practice/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  const [messages, setMessages] = React.useState(["a", "b"]);
+
+  /**
+   * Challenge:
+   * - If there are no unread messages, display "You're all caught up!"
+   * - If there's exactly 1 unread message, it should read "You have
+   *   1 unread message (singular)"
+   * - If there are > 1 unread messages, display "You have <n> unread
+   *   messages" (plural)
+   */
+
+  function determineText() {
+    if (messages.length === 0) {
+      return "You are all caught up!";
+    } else if (messages.length === 1) {
+      return "You have 1 unread messages";
+    } else {
+      return `You have ${messages.length} unread messages`;
+    }
+  }
+
+  return (
+    <div>
+      <h1>{determineText()}</h1>
+    </div>
+  );
+}
